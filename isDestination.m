4209e44a30eda4f3ac9e0f821ac1ec83440265c7
Allen Chan <allen.chan.23x@gmail.com>
2019-10-26 20:22:19 +1100

    update

--- isDestination.m
+++ isDestination.m
@@ -1,4 +1,5 @@
 %% Function to check if we have reached the final destination
+%%
 function bool = isDestination(i,j,iDest,jDest)    
     % Check if we are going to a valid cell
     if (i == iDest) && (j == jDest)
