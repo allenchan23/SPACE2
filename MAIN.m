7ce33eb29a233d0ab84da7f21a3ccf72798aa5d5
Allen Chan <allen.chan.23x@gmail.com>
2019-10-31 15:44:55 +1100

    s

---
+++ MAIN.m
@@ -0,0 +1,23 @@
+%%%%%%%%%%%%%%%%%%%%%%% MAIN %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Read Map 
+% Read and generate map
+% [XYZ_MAP, G_MAP, RES] = getMAP()
+%   - XYZ_MAP - XY Matrix storing Z coordinates for the map
+%   - G_MAP - Binary Matrix storing Z coordinates for the map
+%           - 1 traversable
+%           - 0 not traverable
+%   - RES - resolution between each cell
+
+%% Generate Path
+% - select algorithm :
+%       - A*
+%       - D* lite
+% waypoints = algorithm(XYZ_MAP,G_MAP,RES)
+% waypoints = [x,y] n * 2 x-y coordinates
+
+%% Simulate
+% TBD
+
+%% Animation
+% ""
