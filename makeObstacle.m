3c455d397dfa7a988822ced9ce6b43f991eb1aae
Allen Chan <allen.chan.23x@gmail.com>
2019-11-07 13:39:55 +1100

    algo update

--- makeObstacle.m
+++ makeObstacle.m
@@ -1,4 +1,8 @@
-%% Function for initializing nodes
+%% FUNCTION:  for initializing Obstacles
+% Change current node into an obstacle making it untraversable
+
+%Author: Allen Chan
+
 function V = makeObstacle(V)   
     V.trav = false;               % Boolean - is Traversable?
 end
--- queueInsert.m
+++ queueInsert.m
@@ -1,6 +1,14 @@
+%% FUNCTION - Insert Queue
+% Function to insert into queue
+% Entry - [K1 K1 i j] containing two keys and matrix indexes
+% Return: Void - Updates global queue
+
+% Author: Allen Chan
 function queueInsert(entry)
     
     global QUEUE;
+    
+    % Sort queue
     QUEUE = [entry; QUEUE];
     queueSort();
     
